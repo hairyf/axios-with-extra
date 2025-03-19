ab68c7ee231ded4e8928affc87f5faedfc5671ad
hairyf <wwu710632@gmail.com>
2025-03-20 04:33:53 +0800

    feat: enable exports tests

--- test/exports.test.ts
+++ test/exports.test.ts
@@ -4,7 +4,7 @@ import { getPackageExportsManifest } from 'vitest-package-exports'
 import yaml from 'yaml'
 
 // TODO: remove this when you are ready for the first release
-const IS_READY = false
+const IS_READY = true
 
 describe.runIf(IS_READY)('exports-snapshot', async () => {
   const packages: { name: string, path: string, private?: boolean }[] = JSON.parse(
